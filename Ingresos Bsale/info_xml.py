8f90381fde18dcc40cae2b3189913cdf0562055e
Francisco Moya <franmoyag@gmail.com>
2024-09-12 18:47:04 -0300

    Actualización de Código
    Se modifica almacenamiento de MySql a MongoDB.
    
    Se agrega filtro de facturas.

--- Ingresos Bsale/info_xml.py
+++ Ingresos Bsale/info_xml.py
@@ -1,8 +1,10 @@
 import requests
-import mysql.connector
-from mysql.connector import Error
+from pymongo import MongoClient
 import xml.etree.ElementTree as ET
 import time
+
+# Lista de códigos SII correspondientes a facturas
+factura_codes = ["30", "33", "32", "34", "46", "45", "101", "110"]
 
 
 
@@ -21,24 +23,61 @@ def fetch_and_parse_xml(url_xml, retries=3, delay=5):
             details_list = []
             detalles = root.findall('.//{http://www.sii.cl/SiiDte}Detalle')
 
+
+            # Receptor: información opcional
+            receptor = root.find('.//{http://www.sii.cl/SiiDte}Receptor')
+            receptor_info = {
+                "RUTRecep": receptor.find('{http://www.sii.cl/SiiDte}RUTRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}RUTRecep') is not None else "",
+                "RznSocRecep": receptor.find('{http://www.sii.cl/SiiDte}RznSocRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}RznSocRecep') is not None else "",
+                "GiroRecep": receptor.find('{http://www.sii.cl/SiiDte}GiroRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}GiroRecep') is not None else "",
+                "Contacto": receptor.find('{http://www.sii.cl/SiiDte}Contacto').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}Contacto') is not None else "",
+                "DirRecep": receptor.find('{http://www.sii.cl/SiiDte}DirRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}DirRecep') is not None else "",
+                "CmnaRecep": receptor.find('{http://www.sii.cl/SiiDte}CmnaRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}CmnaRecep') is not None else "",
+                "CiudadRecep": receptor.find('{http://www.sii.cl/SiiDte}CiudadRecep').text if receptor is not None and receptor.find('{http://www.sii.cl/SiiDte}CiudadRecep') is not None else "",
+            }
+
+             # Emisor: información opcional
+            emisor = root.find('.//{http://www.sii.cl/SiiDte}Emisor')
+            emisor_info = {
+                "RUTEmi": emisor.find('{http://www.sii.cl/SiiDte}RUTEmisor').text if emisor is not None and emisor.find('{http://www.sii.cl/SiiDte}RUTEmisor') is not None else "",
+                "RznSocEmi": emisor.find('{http://www.sii.cl/SiiDte}RznSoc').text if emisor is not None and emisor.find('{http://www.sii.cl/SiiDte}RznSoc') is not None else "",
+                "GiroEmi": emisor.find('{http://www.sii.cl/SiiDte}GiroEmis').text if emisor is not None and emisor.find('{http://www.sii.cl/SiiDte}GiroEmis') is not None else "",
+                "Vendedor": emisor.find('{http://www.sii.cl/SiiDte}CdgVendedor').text if emisor is not None and emisor.find('{http://www.sii.cl/SiiDte}CdgVendedor') is not None else "",
+            }
+
+
+            # Transporte: información opcional
+            transporte = root.find('.//{http://www.sii.cl/SiiDte}Transporte')
+            transporte_info = {
+                "DirDest": transporte.find('{http://www.sii.cl/SiiDte}DirDest').text if transporte is not None and transporte.find('{http://www.sii.cl/SiiDte}DirDest') is not None else "",
+                "CmnaDest": transporte.find('{http://www.sii.cl/SiiDte}CmnaDest').text if transporte is not None and transporte.find('{http://www.sii.cl/SiiDte}CmnaDest') is not None else "",
+                "CiudadDest": transporte.find('{http://www.sii.cl/SiiDte}CiudadDest').text if transporte is not None and transporte.find('{http://www.sii.cl/SiiDte}CiudadDest') is not None else "",
+            }
+
+
+            # IdDoc: información opcional
+            id_doc = root.find('.//{http://www.sii.cl/SiiDte}IdDoc')
+            id_doc_info = {
+                "Folio": id_doc.find('{http://www.sii.cl/SiiDte}Folio').text if id_doc is not None and id_doc.find('{http://www.sii.cl/SiiDte}Folio') is not None else "",
+                "FchEmis": id_doc.find('{http://www.sii.cl/SiiDte}FchEmis').text if id_doc is not None and id_doc.find('{http://www.sii.cl/SiiDte}FchEmis') is not None else "",
+            }
+
+
             for detalle in detalles:
-                nro_lin_det = detalle.find('{http://www.sii.cl/SiiDte}NroLinDet')
-                tpo_codigo = detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}TpoCodigo')
-                vlr_codigo = detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}VlrCodigo')
-                nmb_item = detalle.find('{http://www.sii.cl/SiiDte}NmbItem')
-                qty_item = detalle.find('{http://www.sii.cl/SiiDte}QtyItem')
-                prc_item = detalle.find('{http://www.sii.cl/SiiDte}PrcItem')
-                monto_item = detalle.find('{http://www.sii.cl/SiiDte}MontoItem')
-                
                 detail_info = {
-                    "NroLinDet": nro_lin_det.text if nro_lin_det is not None else 0,
-                    "TpoCodigo": tpo_codigo.text if tpo_codigo is not None else "",
-                    "VlrCodigo": vlr_codigo.text if vlr_codigo is not None else "",
-                    "NmbItem": nmb_item.text if nmb_item is not None else "",
-                    "QtyItem": qty_item.text if qty_item is not None else 0.0,
-                    "PrcItem": prc_item.text if prc_item is not None else 0.0,
-                    "MontoItem": monto_item.text if monto_item is not None else 0.0
+                    "NroLinDet": detalle.find('{http://www.sii.cl/SiiDte}NroLinDet').text if detalle.find('{http://www.sii.cl/SiiDte}NroLinDet') is not None else 0,
+                    "TpoCodigo": detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}TpoCodigo').text if detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}TpoCodigo') is not None else "",
+                    "VlrCodigo": detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}VlrCodigo').text if detalle.find('{http://www.sii.cl/SiiDte}CdgItem/{http://www.sii.cl/SiiDte}VlrCodigo') is not None else "",
+                    "NmbItem": detalle.find('{http://www.sii.cl/SiiDte}NmbItem').text if detalle.find('{http://www.sii.cl/SiiDte}NmbItem') is not None else "",
+                    "QtyItem": detalle.find('{http://www.sii.cl/SiiDte}QtyItem').text if detalle.find('{http://www.sii.cl/SiiDte}QtyItem') is not None else 0.0,
+                    "PrcItem": detalle.find('{http://www.sii.cl/SiiDte}PrcItem').text if detalle.find('{http://www.sii.cl/SiiDte}PrcItem') is not None else 0.0,
+                    "MontoItem": detalle.find('{http://www.sii.cl/SiiDte}MontoItem').text if detalle.find('{http://www.sii.cl/SiiDte}MontoItem') is not None else 0.0,
+                    **receptor_info,
+                    **transporte_info,
+                    **id_doc_info,
+                    **emisor_info
                 }
+
                 details_list.append(detail_info)
 
             return details_list
@@ -57,94 +96,47 @@ def fetch_and_parse_xml(url_xml, retries=3, delay=5):
             return []
 
 
+# Guardar detalles en MongoDB
+def save_details_to_mongo():
+    client = MongoClient('localhost', 27017)
+    db = client['compras_bsale']
+    collection_bsale = db['bsale_documents']
+    collection_details = db['bsale_details']
 
+    # Leer los IDs y URLs desde la colección bsale y filtrar solo facturas (codeSii)
+    records = collection_bsale.find(
+        {"codeSii": {"$in": factura_codes}},  # Filtrar solo facturas
+        {"number": 1, "urlXml": 1}
+    )
 
-def save_details_to_mysql(host, database, user, password):
-    connection = None
+    for record in records:
+        number_doc = record["number"]
+        urlXml = record.get("urlXml", "")
 
-    try:
-        connection = mysql.connector.connect(
-            host=host,
-            database=database,
-            user=user,
-            password=password
-        )
-        
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS bsale_details (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                bsale_id INT,
-                NroLinDet INT,
-                TpoCodigo VARCHAR(255),
-                VlrCodigo VARCHAR(255),
-                NmbItem VARCHAR(255),
-                QtyItem DECIMAL(10, 2),
-                PrcItem DECIMAL(10, 2),
-                MontoItem DECIMAL(10, 2),
-                UNIQUE(bsale_id, NroLinDet),
-                FOREIGN KEY (bsale_id) REFERENCES bsale(id)
+        if not urlXml:
+            print(f"Documento {number_doc} tiene URL vacía, omitiendo...")
+            continue
+
+        xml_details = fetch_and_parse_xml(urlXml)
+
+        if not xml_details:
+            print(f"No se encontraron detalles para Documento {number_doc}, omitiendo...")
+            continue
+
+        for detail in xml_details:
+            detail['number'] = number_doc
+            print(f"Insertando/actualizando en MongoDB: {detail}")
+
+            collection_details.update_one(
+                {"number": number_doc, "NroLinDet": detail['NroLinDet']},
+                {"$set": detail},
+                upsert=True
             )
-            """)
-            
-            detail_insert_query = """
-            INSERT INTO bsale_details (
-                bsale_id, NroLinDet, TpoCodigo, VlrCodigo, NmbItem, QtyItem, PrcItem, MontoItem
-            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE
-                TpoCodigo=VALUES(TpoCodigo),
-                VlrCodigo=VALUES(VlrCodigo),
-                NmbItem=VALUES(NmbItem),
-                QtyItem=VALUES(QtyItem),
-                PrcItem=VALUES(PrcItem),
-                MontoItem=VALUES(MontoItem)
-            """
-            
 
-            # Leer los IDs y URLs de la tabla bsale
-            cursor.execute("SELECT id, urlXml FROM bsale")
-            records = cursor.fetchall()
-            
-            for record in records:
-                bsale_id = record[0]
-                urlXml = record[1]
-                
-                if not urlXml:
-                    print(f"ID {bsale_id} tiene URL vacía, omitiendo...")
-                    continue
-                
-                xml_details = fetch_and_parse_xml(urlXml)
-                
-                if not xml_details:
-                    print(f"No se encontraron detalles para bsale_id {bsale_id}, omitiendo...")
-                    continue
-                
-                for detail in xml_details:
-                    #Imprimir los valores que se insertan en detalles
-                    print("Insertando/actualizando en bsale_details:", (
-                        bsale_id, detail['NroLinDet'], detail['TpoCodigo'], detail['VlrCodigo'], detail['NmbItem'], 
-                        detail['QtyItem'], detail['PrcItem'], detail['MontoItem']
-                    ))
-                    
-                    cursor.execute(detail_insert_query, (
-                        bsale_id, detail['NroLinDet'], detail['TpoCodigo'], detail['VlrCodigo'], detail['NmbItem'], 
-                        detail['QtyItem'], detail['PrcItem'], detail['MontoItem']
-                    ))
-                
-            connection.commit()
-            print("Detalles insertados/actualizados en la base de datos MySQL.")
-    
-    except Error as e:
-        print(f"Error al conectar a MySQL: {e}")
-    
-    finally:
-        if connection.is_connected():
-            cursor.close()
-            connection.close()
-            print("Conexión a MySQL cerrada.")
-            
+    print("Detalles insertados/actualizados en la base de datos MongoDB.")
+    client.close()
 
-save_details_to_mysql(host='localhost', database='api_myst', user='api_user', password='kami.2024.sushi')
+# Llamada a la función para guardar en MongoDB
+save_details_to_mongo()
 
 print("Proceso Finalizado.....")
