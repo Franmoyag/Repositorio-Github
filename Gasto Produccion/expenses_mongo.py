e41d5d4c7960ee1720c747fbbc1f41be0394250f
Francisco Moya <franmoyag@gmail.com>
2024-10-01 17:56:02 -0300

    Actualización.

--- Gasto Produccion/expenses_mongo.py
+++ Gasto Produccion/expenses_mongo.py
@@ -20,7 +20,7 @@ POSTGRES_PASS = "j4mdl9to5sd"
 # INTERNAL SERVER PARAMETERS
 
 MONGO_HOST = "127.0.0.1"
-MONGO_DB = "data_bsale"
+MONGO_DB = "production_expenses" #Base de Datos segun necesidad.
 MONGO_COLLECTION = "wasabi_expenses"
 MONGO_USER = ""
 MONGO_PASS = ""
